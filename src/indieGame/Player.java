d938cc87ffb762c9dbb0e90c4d1e56961779bc23
UserPL022Pc11 <UserPL022Pc11@SLFRPL022PC11.ensino.atec.local>
2016-11-08 10:59:36 +0000

    Teste

---
+++ src/indieGame/Player.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indieGame;
+
+/**
+ *
+ * @author UserPL022Pc11
+ */
+public class Player extends SensibleDummy{
+    int fatigue, atack, defense;
+    
+    char[] faceTo = {'N','S','E','W'};
+    
+    public void pass(){
+        
+    }
+    
+    public void shoot(){
+        
+    }
+}
